61eb757317863192b48118585c998de8337d7c57
ACGNnsj <ootopoo@vip.qq.com>
2023-03-22 18:56:48 +0800

    Create acgmn-remove-bilibili-live-gift-panel-update.js
    Init

---
+++ acgmn-remove-bilibili-live-gift-panel-update.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Remove Bilibili Live Gift Panel
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove gift panel from bilibili live page
+// @author       ACGMN
+// @updateURL    https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-live-gift-panel-update.js
+// @match        *://live.bilibili.com/*
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://live.bilibili.com&size=64
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    setInterval(function () {
+        if(document.querySelector('#web-player__bottom-bar__container')){
+            document.querySelector('#web-player__bottom-bar__container').remove()
+        }
+        if(document.querySelector('#gift-control-vm')){
+            document.querySelector('#gift-control-vm').remove()
+        }
+    }, 1000)
+})();
