1d4337e1696e98764615317f0e45565687f99829
ACGNnsj <ootopoo@vip.qq.com>
2023-04-25 11:52:33 +0800

    Update acgmn-remove-bilibili-webp-suffix-update.js

--- acgmn-remove-bilibili-webp-suffix-update.js
+++ acgmn-remove-bilibili-webp-suffix-update.js
@@ -5,6 +5,8 @@
 // @description  Remove gift panel from bilibili live page 去除b站图片webp后缀
 // @author       ACGMN
 // @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-webp-suffix-update.js
+// @updateURL    https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-webp-suffix-update.js
 // @match        *://i0.hdslb.com/*
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://i0.hdslb.com&size=64
 // @grant        none
