f0b1b0d45712d7e2e37c7053245293dfdecf7cdc
Shijie Ni <ootopoo@vip.qq.com>
2023-07-04 15:44:58 +0800

    Update acgmn-remove-bilibili-live-gift-panel-update.js

--- acgmn-remove-bilibili-live-gift-panel-update.js
+++ acgmn-remove-bilibili-live-gift-panel-update.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Remove Bilibili Live Gift Panel
 // @namespace    ACGMN
-// @version      0.1.2
+// @version      0.1.3
 // @description  Remove gift panel from bilibili live page 去除b站直播间礼物面板
 // @author       ACGMN
 // @license      MIT
-// @downloadURL  https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-live-gift-panel-update.js
-// @updateURL    https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-live-gift-panel-update.js
+// @downloadURL  https://raw.githubusercontent.com/ACGNnsj/TampermonkeyScripts/main/acgmn-remove-bilibili-live-gift-panel-update.js
+// @updateURL    https://raw.githubusercontent.com/ACGNnsj/TampermonkeyScripts/main/acgmn-remove-bilibili-live-gift-panel-update.js
 // @match        *://live.bilibili.com/*
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://live.bilibili.com&size=64
 // @grant        none
