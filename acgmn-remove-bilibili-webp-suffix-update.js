6e8554b40dd74ec070cdd44755c2167137b8913e
Shijie Ni <ootopoo@vip.qq.com>
2023-07-04 15:45:34 +0800

    Update acgmn-remove-bilibili-webp-suffix-update.js
    Update URL

--- acgmn-remove-bilibili-webp-suffix-update.js
+++ acgmn-remove-bilibili-webp-suffix-update.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Remove Bilibili WebP Suffix
 // @namespace    ACGMN
-// @version      0.1.1
+// @version      0.1.2
 // @description  Remove gift panel from bilibili live page 去除b站图片webp后缀
 // @author       ACGMN
 // @license      MIT
-// @downloadURL  https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-webp-suffix-update.js
-// @updateURL    https://raw.githubusercontent.com/ACGNnsj/RemoveBilibiliLiveGiftPanel/main/acgmn-remove-bilibili-webp-suffix-update.js
+// @downloadURL  https://raw.githubusercontent.com/ACGNnsj/TampermonkeyScripts/main/acgmn-remove-bilibili-webp-suffix-update.js
+// @updateURL    https://raw.githubusercontent.com/ACGNnsj/TampermonkeyScripts/main/acgmn-remove-bilibili-webp-suffix-update.js
 // @match        *://i0.hdslb.com/*
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://i0.hdslb.com&size=64
 // @grant        none
